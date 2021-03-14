1d0bcc1dba5d2ab50fa0649f24fa657b1dee7ceb
sam-eng <sme388@nyu.edu>
2021-03-13 22:38:46 -0500

    add link for Meet the Team page

--- front-end/src/Footer.js
+++ front-end/src/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
     <div id="footer-container">
-      <Link to="/terms-of-service">Terms of Service</Link>
+      <Link to="/terms-of-service" className="footer-links">Terms of Service</Link>
+      <Link to="/meet-the-team" className="footer-links">Meet the Team</Link>
     </div>
   );
 }
