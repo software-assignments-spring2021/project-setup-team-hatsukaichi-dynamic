d271e8ba670c1eb2b6249157654826926e0ef2ad
sam-eng <sme388@nyu.edu>
2021-03-15 21:49:46 -0400

    connect to mockaroo api

--- front-end/src/Profile.js
+++ front-end/src/Profile.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react'
 import Footer from './Footer';
 import axios from 'axios';
 
-function UserInfo(props) {
+function UserInfo( {data} ) {
   return (
     <>
-      {props.user === undefined ? "empty's" : props.user + "'s"}
+      {data.username === undefined ? "empty's" : data.username + "'s"}
       <span> profile page! </span>
     </>
   );
@@ -16,11 +16,26 @@ const Profile = (props) => {
 
   useEffect(() => {
     axios(`https://my.api.mockaroo.com/tv_users/${props.id}.json?key=71236df0`)
-  });
+      .then( (response) => {
+        console.log(response.data)
+        setUserData(response.data)
+      })
+      .catch( (err) => {
+        // This case is likely to be due to Mockaroo rate limiting!
+        // It'd be good to add some error handling here later, if someone tries to 
+        // access a non-existent user
+        console.log(err);
+
+        setUserData(null);
+      });
+  }, [props.id]);
   
   return (
     <>
-      <p>Hello!</p>
+      {userData === null 
+        ? <p>Oh no! Looks like this user wasn't found....</p>
+        : <UserInfo data={userData} />
+      }
       <Footer />
     </>
   )
