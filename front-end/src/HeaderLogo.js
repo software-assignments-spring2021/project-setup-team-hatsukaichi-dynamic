255f6a64a60264cf10b3f6f85df200e6a392c4b3
stovalln21 <78505502+stovalln21@users.noreply.github.com>
2021-03-21 12:11:43 -0400

    created image folder (#121)
    * created image folder
    
    * renamed stock photo image

--- front-end/src/HeaderLogo.js
+++ front-end/src/HeaderLogo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import logo from './logo.png'
+import logo from './Images/logo.png'
 import './HeaderLogo.css'
 
 function HeaderLogo() {
