b39f89ae4d5ec0fae4e4a111c80975f4409adfd7
sam-eng <sme388@nyu.edu>
2021-03-21 18:46:55 -0400

    add more styling work for Signup page

--- front-end/src/Signup.js
+++ front-end/src/Signup.js
@@ -7,12 +7,15 @@ import './Signup.css';
 
 function Signup() {
   const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [passwordMismatch, setPasswordMismatch] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("submit!");
+    
+    password !== passwordConfirm ? setPasswordMismatch(true) : setPasswordMismatch(false);
   }
 
   return (
@@ -23,18 +26,30 @@ function Signup() {
           <h2>Sign up for TV Tracker</h2>
           <div className="form-fields">
             <label>Username</label>
-            <input type="text" name="username" value={username} onChange={(e) => {setUsername(e.target.value)}}/>
+            <input type="text" name="username" value={username} onChange={(e) => {setUsername(e.target.value)}} required/>
+            <label>Email</label>
+            <input type="text" name="email" value={email} onChange={(e) => {setEmail(e.target.value)}} required/>
             <label>Password</label>
-            <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
             <label>Confirm Password</label>
-            <input type="password" name="passwordconfirm" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input 
+              type="password" 
+              name="passwordconfirm" 
+              value={passwordConfirm} 
+              onChange={(e) => setPasswordConfirm(e.target.value)} 
+              required
+            />
+            {passwordMismatch ? 
+              <p className="error message">
+                Could not create account--passwords did not match.
+              </p>
+              : null}
           </div>
           <button id="signup-button" type="submit">Sign Up</button>
           <p className="message">Already have an account? <Link to="/login">Log In</Link></p>
         </form>
       </div>
       <Footer />
-
     </>
   );
 }
