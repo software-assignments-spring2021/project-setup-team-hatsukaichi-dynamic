a81da8008cf29595e1466991ae9338976a6ea23e
Mason Kalaty <mk6578@nyu.edu>
2021-03-21 12:07:21 -0400

    Sticky Header Fixes

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -3,24 +3,20 @@ import StickyHeader from 'react-sticky-header';
 import './Header.css' 
 import Hamburger from './Hamburger'
 
-const Header = () => (
-  [
-  <Hamburger pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />,
+const Header = () => {
+  return (
   <StickyHeader    
     // This section is sticky 
     header={
       <div className="header-root">
+        <Hamburger pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
         <h1 className="header-title">TV Tracker</h1>
-        <Hamburger>1</Hamburger>
       </div>
     }
   >
-    <section>
-      <p><br></br><br></br><br></br><br></br></p>
-    </section>
   </StickyHeader>
   // The <section> right above me is a 'barrier' so the header has space
-  ]
-);
+  )
+}
 
 export default Header;
