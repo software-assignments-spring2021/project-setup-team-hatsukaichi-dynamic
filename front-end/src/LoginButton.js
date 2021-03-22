be8465df826a3d0f6bc31059ede62bfa44aecaec
LevBernstein <lev2000@me.com>
2021-03-22 14:25:49 -0400

    CSS fixes

--- front-end/src/LoginButton.js
+++ front-end/src/LoginButton.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import './LoginButton.css'
 
 
 function LoginButton() {
   return (
 	<Link to={'/Login'}>
-  		<button> Login </button>
+  		<button className="loginButton"> Login </button>
   	</Link>
   );
 }
--- front-end/src/SignupButton.js
+++ front-end/src/SignupButton.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import './SignupButton.css'
 
 
 function SignupButton() {
   return (
 	<Link to={'/Signup'}>
-  		<button> Signup </button>
+  		<button className="signupButton"> Sign Up </button>
   	</Link>
   );
 }
