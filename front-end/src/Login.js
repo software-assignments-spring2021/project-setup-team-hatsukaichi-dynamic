ca0ef88d6c6468a975ab2fea31dd44e4f1c9e09b
LevBernstein <lev2000@me.com>
2021-03-21 23:40:15 -0400

    Login looks like a page now

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -1,42 +1,40 @@
 import './Login.css';
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
-import Hamburger from './Hamburger';
+import { Link, useHistory } from 'react-router-dom';
+import Header from './Header';
 import Footer from './Footer'
 
 
-export default function Login(){
-
+function Login(){
+    const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
-function handleChange(e) {
-
-}
+    const history = useHistory();
 
 function submit() {
-   
+    history.push('/profile/1')
 }
-
-function Login() {
     return (
-        <div>
-            <Hamburger pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />  
-            <div className="login">
-                <div className="form">
-                    <form className="login-form" onSubmit={(e) => { submit(); e.preventDefault(); }}>
-                        <img id="logo" src="/logo.svg" />
-                        <input autoFocus={true} type="email" name="username" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
-                        <input type="password" name="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                        <button type="submit">login</button>
-                        <p className="message">Not registered? <Link to="/Signup">Create an account</Link></p>
-                    </form>
-                </div>
-            </div>
-        <Footer/> 
-
-    </div>
-
-        );
-    }
+        <>
+	    <Header />  
+	    <div id="login-container">
+		<form id="login-form" onSubmit={(e) => { submit(); e.preventDefault(); }}>
+		    <h2>Log In to TV Tracker</h2>
+		    <div className="form-fields">
+			<label>Username</label>
+			<input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+			<label>Email</label>
+			<input autoFocus={true} type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+			<label>Password</label>
+			<input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+			<button id="login-button" type="submit">Login</button>
+			<p className="message">Not registered? <Link to="/Signup">Create an account</Link></p>
+		    </div>    
+		</form>
+		<Footer/> 
+	    </div>
+	</>	
+    );
 }
+
+export default Login;
