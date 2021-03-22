d456dda3df48c4630d1a8378374b7e5387bb5563
Mason Kalaty <mk6578@nyu.edu>
2021-03-22 01:01:31 -0400

    logo in sticky header

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -2,22 +2,20 @@ import 'react-sticky-header/styles.css';
 import StickyHeader from 'react-sticky-header';
 import './Header.css'
 import Hamburger from './Hamburger'
+import logo from './logo.png'
 
 const Header = () => {
   return (
     <StickyHeader
-      // This section is sticky 
       header={
         <div className="header-root">
           <Hamburger pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-          <h1 className="header-title">TV Tracker</h1>
+          <img src={logo} className="logo-img" />
         </div>
       }
     >
     </StickyHeader>
   );
 }
-
-// The <section> right above me is a 'barrier' so the header has space
 
 export default Header;
