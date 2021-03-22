80d4b466b4dd49f51d733cf7fdca1b84f18f21c7
Ben Kaplan <benrkaplan@gmail.com>
2021-03-22 13:57:41 -0400

    fixing buttons

--- front-end/src/SignupButton.js
+++ front-end/src/SignupButton.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 function SignupButton() {
   return (
 	<Link to={'/Signup'}>
-  		<button> Login </button>
+  		<button> Signup </button>
   	</Link>
   );
 }
