789c19f2bc38332e5fe1c877e0308919ef05fbf7
Sam Eng <35475933+sam-eng@users.noreply.github.com>
2021-03-24 16:29:02 -0400

    Create GET route for individual show (#156)

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,15 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const axios = require("axios")
+const app = express()
+
+app.get('/shows/:id', (req, res, next) => {
+    axios.get(`https://my.api.mockaroo.com/shows/${req.params.id}.json?key=`)
+        .then( (response) => {
+            res.json(response.data)
+        })
+        .catch( (err) => {
+            next(err)
+        })
+});
 
 module.exports = app
