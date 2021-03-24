98e9f1990ee44f3711e4a426976881060c175551
Ben <43765521+benrkaplan@users.noreply.github.com>
2021-03-24 13:21:40 -0400

    Issue 101 setup backend server spike (#153)
    * setting up backend server
    
    * adding further dependencies

---
+++ back-end/server.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env/ node 
+
+//loading the web server
+const server = require("./app");
+
+//the port to listen to for incoming requests
+const port = 3000
+
+//call to start listening
+const listener = server.listen(port, funciton () {
+	console.log('Server running on port: ${port}')
+})
+
+//funciton to close listener
+const close = () => {
+	listener.close(),
+}
+
+module.exports = {
+	close: close,
+}
