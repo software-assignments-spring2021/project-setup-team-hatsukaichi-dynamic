f53e846ad6aaafefc73dc7fa0c92191d202dbfd1
Mason Kalaty <mk6578@nyu.edu>
2021-03-26 15:29:44 -0400

    Get users route

--- back-end/app.js
+++ back-end/app.js
@@ -22,4 +22,14 @@ app.get('/tv_users/:id', (req, res, next) => {
         })
 });
 
+app.get('/tv_users', (req, res, next) => {
+    axios.get(`https://my.api.mockaroo.com/tv_users.json?key=`)
+        .then( (response) => {
+            res.json(response.data)
+        })
+        .catch( (err) => {
+            next(err)
+        })
+});
+
 module.exports = app
