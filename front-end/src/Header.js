75077fbadfecb2d876024c65bb732a1defaac6ec
sam-eng <sme388@nyu.edu>
2021-03-26 09:20:44 -0400

    alternate log in/sign up with logout button

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -7,18 +7,27 @@ import logo from './Images/logo.png'
 import HeaderLogo from './HeaderLogo';
 import SignupButton from './SignupButton';
 import LoginButton from './LoginButton'
+import { AuthContext } from './App';
+import LogoutButton from './LogoutButton';
 
 const Header = () => {
+  const { loggedInUser } = React.useContext(AuthContext);
   return (
     <StickyHeader
       header={
         <div className="header-root">
           <Hamburger pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
           <HeaderLogo src={logo} className="logo-img" />
-	  <div className="login-signup-buttons">
-	    <LoginButton />
-	    <SignupButton />
-	  </div>
+          <div className="login-signup-buttons">
+            {loggedInUser 
+            ? <LogoutButton /> 
+            :
+              <>
+                <LoginButton />
+                <SignupButton />
+              </>
+            }
+          </div>
         </div>
       }
     >
