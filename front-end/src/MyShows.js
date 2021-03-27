5a62ffede877f7ffe5258499209f872eda4004d4
Almazhan (Alma) Kapan <aa5456@nyu.edu>
2021-03-28 01:25:07 +0800

    Issue 168 final styling fixes (#174)
    * add css quer-s, respons style for MyShows,Home,Login
    
    * add css quer, style for Profile com,Settings modal
    
    * css quer., respons design for Individ Show, Terms, Footer
    
    * add css quer-s,resp design to Login,Signup,MeetTeam
    
    * add css queries, responsive design for the pages
    
    * fix overlay in Settings modal, make link responsive
    
    * respond to reviews
    
    * fix comments, format all files with linter
    
    * minor fix
    
    * Revert "fix comments, format all files with linter" - after team discussion
    This reverts commit 3714f04825a861fb022359ed6657649befe19dc9.
    
    * fix settings modal overlay, edit spacing
    
    * fix vscode
    
    * fixing class to className
    
    * edit in progress, completed buttons
    
    * fix toggleData bug for Profile.js

--- front-end/src/MyShows.js
+++ front-end/src/MyShows.js
@@ -67,7 +67,7 @@ const ShowGrid = (props) => {
           ? filteredShows.map((show) => {
               return <Link to={`/show/${show.id}`} key={show.id}> <img className="show-images" src={mockShowImage(show.id)} alt={`cover-${show.id}`}/></Link>
           })
-          : <p>No shows found...</p>
+          : <p id="no-shows">No shows found...</p>
         }
 
       </div>
@@ -160,7 +160,7 @@ const MyShows = (props) => {
         return searchShows(input, response.data);
       })
      .catch((err) => {
-	console.log("We likely reached Mockaroo's request limit, or you did not insert your API key in .env.");
+  console.log("We likely reached Mockaroo's request limit, or you did not insert your API key in .env.");
         console.log(err);
         return searchShows(input, mockAllShows);
       });
@@ -191,14 +191,15 @@ const MyShows = (props) => {
     <>
       <Header />
       <div id="container">
-        <h3>{userData.username}'s Shows</h3>
+        <h3 id="profile-title">{userData.username}'s Shows</h3>
         {/* TODO: Use onChange props for AsyncSelect to trigger Individual Show modal */}
         <div id="search-container">
           <AsyncSelect id="search-bar" cacheOptions defaultOptions loadOptions={loadOptions} onChange={linkToShow}/>
         </div>
         <div id="filter-container">
-          <button
+          <button 
             className={inProgressSelected ? "selected filter-button" : "filter-button"}
+
             onClick={(e) => onStatusChange("in progress")}
           >
             In Progress
@@ -209,9 +210,13 @@ const MyShows = (props) => {
             onRequestClose={toggleModal}
             contentLabel="Filter Shows"
           >
-            <h1>Filter by Platform</h1>
+             <div className="modal-contents">
+               
+            <h3 id="filter-title" >Filter by Platform</h3>
+            <br/>
             <Select options={platforms} onChange={onChange} value={selectedPlatform} />
-            <button className="my-shows-button" onClick={toggleModal}>Apply</button>
+            <button className="my-shows-button" id="apply" onClick={toggleModal}>Apply</button>
+            </div>
           </Modal>
           <button
             className={completedSelected ? "selected filter-button" : "filter-button"}
@@ -228,3 +233,4 @@ const MyShows = (props) => {
 }
 
 export default MyShows
+
