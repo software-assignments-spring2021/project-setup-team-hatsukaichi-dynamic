f957f08ffc38e9fd5178d7e2c7c7255e2580608d
Almazhan Kapan <aa5456@nyu.edu>
2021-03-29 06:53:00 +0800

    specify API keys for routes

--- back-end/app.js
+++ back-end/app.js
@@ -3,7 +3,7 @@ const axios = require("axios")
 const app = express()
 
 app.get('/shows/:id', (req, res, next) => {
-    axios.get(`https://my.api.mockaroo.com/shows/${req.params.id}.json?key=`)
+    axios.get(`https://my.api.mockaroo.com/shows/${req.params.id}.json?key=${process.env.REACT_APP_MOCKAROO_KEY}`)
         .then( (response) => {
             res.json(response.data)
         })
@@ -13,7 +13,7 @@ app.get('/shows/:id', (req, res, next) => {
 });
 
 app.get('/tv_users/:id', (req, res, next) => {
-    axios.get(`https://my.api.mockaroo.com/tv_users/${req.params.id}.json?key=`)
+    axios.get(`https://my.api.mockaroo.com/tv_users/${req.params.id}.json?key=${process.env.REACT_APP_MOCKAROO_KEY}`)
         .then( (response) => {
             res.json(response.data)
         })
