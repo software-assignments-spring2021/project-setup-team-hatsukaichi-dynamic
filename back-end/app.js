aa9e4268a0b9d962d8c9b6267f2bac3628ecdb3e
Almazhan Kapan <aa5456@nyu.edu>
2021-03-30 03:16:26 +0800

    include bodyparser middleware

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const axios = require("axios")
 const app = express()
+app.use(express.json()) // use the bodyparser middleware
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/shows/:id', (req, res, next) => {
     axios.get(`https://my.api.mockaroo.com/shows/${req.params.id}.json?key=`)
