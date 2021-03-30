04398925e49fa1e1b3b41d2f9411bbcc166800fe
Almazhan Kapan <aa5456@nyu.edu>
2021-03-31 05:06:25 +0800

    add dependencies

--- back-end/app.js
+++ back-end/app.js
@@ -3,7 +3,7 @@ const axios = require("axios") // middleware for API integration
 const app = express()
 const morgan = require("morgan") // middleware for logging of incoming HTTP requests
 require('dotenv').config();
-app.use(morgan("dev")) 
+app.use(morgan("dev")) // dev is a concise color-coded default style for morgan
 
 app.get('/shows/:id', (req, res, next) => {
     axios.get(`https://my.api.mockaroo.com/shows/${req.params.id}.json?key=${process.env.MOCKAROO_KEY}`)
