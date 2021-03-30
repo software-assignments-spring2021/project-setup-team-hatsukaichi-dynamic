2410870a2e512635ce481941554dcf8f0dfdfa5e
stovalln21 <78505502+stovalln21@users.noreply.github.com>
2021-03-30 14:15:44 -0400

    Created patch route #106 (#195)
    * created patch route
    
    * changed variable name

--- back-end/app.js
+++ back-end/app.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const axios = require("axios")
 const app = express()
-require('dotenv').config();
+require('dotenv').config()
+app.use(express.json())
+
+
 
 app.get('/shows/:id', (req, res, next) => {
     axios.get(`https://my.api.mockaroo.com/shows/${req.params.id}.json?key=${process.env.MOCKAROO_KEY}`)
@@ -32,5 +35,22 @@ app.get('/shows', (req, res, next) => {
             next(err)
         })
 });
+
+app.patch('/tv_users/:id', (req, res, next) => {
+    axios.patch(`https://my.api.mockaroo.com/tv_users/${req.params.id}.json?key=${process.env.MOCKAROO_KEY}&__method=PATCH`, {
+        "username": (req.body.username == null ? req.query.username : req.body.username),
+        "password": (req.body.password == null ? req.query.password : req.body.password),
+        "bio": (req.body.bio == null ? req.query.bio : req.body.bio),
+        "shows": (req.body.shows == null ? req.query.shows : req.body.shows),
+    })
+    .then((response) => {
+        console.log(response)
+        res.json(response.data)
+    })
+        .catch((err) => {
+            next(err)
+        })
+});
+
 
 module.exports = app
