9f1099efff80c941941e74f683559230ad356382
Mason Kalaty <mk6578@nyu.edu>
2021-04-01 10:33:31 -0400

    url changes

--- back-end/app.js
+++ back-end/app.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 app.use(express.json())
 
 app.get('/tv_users', (req, res, next) => {
-    axios.get(`https://my.api.mockaroo.com/tv_users.json?key=`)
+    axios.get(`https://my.api.mockaroo.com/tv_users.json?key=${process.env.MOCKAROO_KEY}`)
         .then( (response) => {
             res.json(response.data)
         })
