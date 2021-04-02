ba00c53fbfc4b3f419e179b6a2f5ecf667acd3ea
Almazhan (Alma) Kapan <aa5456@nyu.edu>
2021-04-03 02:57:29 +0800

    Issue 105 post users route (#201)
    * add post request to users
    
    * include bodyparser middleware
    
    * fix dotenv file
    
    * change names of the api keys in the .env

--- back-end/app.js
+++ back-end/app.js
@@ -4,6 +4,7 @@ const app = express()
 const morgan = require("morgan") // middleware for logging of incoming HTTP requests
 require('dotenv').config()
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan("dev")) // dev is a concise color-coded default style for morgan
 
 app.get('/tv_users', (req, res, next) => {
@@ -45,6 +46,21 @@ app.get('/tv_users/:id', (req, res, next) => {
       next(err)
     })
 })
+
+app.post('/tv_users', (req, res, next) => {
+    axios.post(`https://my.api.mockaroo.com/tv_users.json?key=${process.env.API_KEY_MOCKAROO}&__method=POST`, {
+        "username": req.body.username,
+        "email": req.body.email,
+        "password": req.body.password
+    })
+    .then((response) => {
+        console.log(response)
+        res.json(response.data)
+    })
+    .catch((err) => {
+        next(err)
+    })
+});
 
 app.get('/shows', (req, res, next) => {
   axios
