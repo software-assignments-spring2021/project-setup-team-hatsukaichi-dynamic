3aacfec9ca9931de2921eb7799742171b0d3e5d2
sam-eng <sme388@nyu.edu>
2021-04-04 14:12:42 -0400

    add unit test for get shows

--- back-end/test/getShows.js
+++ back-end/test/getShows.js
@@ -1,15 +1,22 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
-const nock = require('nock')
+const axios = require('axios')
 const sinon = require('sinon')
 const expect = chai.expect
+const server = require('../app.js')
 chai.use(chaiHttp)
 
-const hello = () => 'hello world';
-
-describe('hello function', () => {
-  it('should return hello world', () => {
-    const res = hello()
-    expect(res).to.equal('hello world')
+describe('GET /shows', () => {
+  let stub;
+  before(() => {
+    stub = sinon.stub(axios, 'get').resolves({data: ['show one', 'show two']})
+  })
+  after(() => {
+    stub.restore()
+  })
+  it('should return 200 OK', async () => {
+    chai.request(server).get('/shows').then( res => {
+      expect(res.status).to.equal(200)
+    })
   })
 })
