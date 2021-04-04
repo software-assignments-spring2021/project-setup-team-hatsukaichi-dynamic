7760b97d632ba549d1d1f9102789b645a002a678
sam-eng <sme388@nyu.edu>
2021-04-04 15:29:51 -0400

    add assertion for returned data

--- back-end/test/getShows.js
+++ back-end/test/getShows.js
@@ -8,9 +8,12 @@ chai.use(chaiHttp)
 
 describe('GET /shows', () => {
   it('should return 200 OK', async () => {
-    const stub = sinon.stub(axios, 'get').resolves(true)
+    // This effectively makes response.data in the show route equal to 
+    // {id: 1, name: 'sample show}
+    const stub = sinon.stub(axios, 'get').resolves({data: {id: 1, name: 'sample show'}})
     const res = await chai.request(server).get('/shows')
     expect(res.status).to.equal(200)
+    expect(res.body).to.deep.equal({id: 1, name: 'sample show'})
     stub.restore()
   })
   it('should return 500 error', async () => {
