d0d5c9a5a3b7e9e47ae9eabe1f224f2b71f461d7
Mason Kalaty <mk6578@nyu.edu>
2021-04-05 10:37:01 -0400

    run test successfully

--- back-end/test/getShow.js
+++ back-end/test/getShow.js
@@ -6,18 +6,12 @@ const expect = chai.expect
 const server = require('../app.js')
 chai.use(chaiHttp)
 
-describe('GET /show', () => {
+describe('GET /shows/1', () => {
   it('should return 200 OK', async () => {
     const stub = sinon.stub(axios, 'get').resolves({data: {id: 1, name: 'sample show'}})
-    const res = await chai.request(server).get('/show')
+    const res = await chai.request(server).get('/shows/1')
     expect(res.status).to.equal(200)
     expect(res.body).to.deep.equal({id: 1, name: 'sample show'})
-    stub.restore()
-  })
-  it('should return 500 error', async () => {
-    const stub = sinon.stub(axios, 'get').rejects(true)
-    const res = await chai.request(server).get('/show')
-    expect(res.status).to.equal(500)
     stub.restore()
   })
 })
