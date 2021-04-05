f55425d286be3fe43146abe35dac18951061cd0d
Almazhan Kapan <aa5456@nyu.edu>
2021-04-05 08:21:21 +0800

    debug and fix 403,406 errors, edit headers, params

--- back-end/app.js
+++ back-end/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 const morgan = require("morgan") // middleware for logging of incoming HTTP requests
-require('dotenv').config()
+require('dotenv').config({ silent: true })
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan("dev")) // dev is a concise color-coded default style for morgan
@@ -49,9 +49,9 @@ app.get('/tv_users/:id', (req, res, next) => {
 
 app.post('/tv_users', (req, res, next) => {
     axios.post(`https://my.api.mockaroo.com/tv_users.json?key=${process.env.API_KEY_MOCKAROO}&__method=POST`, {
-        "username": req.body.username,
-        "email": req.body.email,
-        "password": req.body.password
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
     })
     .then((response) => {
         console.log(response)
@@ -101,7 +101,13 @@ app.get('/shows-trakt', (req, res, next) => {
   if (Object.keys(req.query).length === 0){
     axios
       .get(
-        `https://api.trakt.tv/shows/popular?Content-Type=application/json&trakt-api-version=2&trakt-api-key=${process.env.API_KEY_TRAKT}`
+        `https://api.trakt.tv/shows/popular`,{
+          headers: {
+            'trakt-api-version': 2, 
+            'trakt-api-key':`${process.env.API_KEY_TRAKT}`,
+            'Accept': '*/*'
+          }
+        }
       )  
       .then((response) => {
         res.json(response.data)
@@ -113,7 +119,13 @@ app.get('/shows-trakt', (req, res, next) => {
   }else{
     axios
       .get(
-        `https://api.trakt.tv/search/show,movie?query=${req.query.query}&Content-Type=application/json&trakt-api-version=2&trakt-api-key=${process.env.API_KEY_TRAKT}`
+        `https://api.trakt.tv/search/show,movie?query=${req.query.query}&Content-Type=application/json&trakt-api-version=2&trakt-api-key=${process.env.API_KEY_TRAKT}`,{
+          headers: {
+            'trakt-api-version': 2, 
+            'trakt-api-key':`${process.env.API_KEY_TRAKT}`,
+            'Accept': '*/*'
+          }
+        }
       )
       .then((response) => {
         res.json(response.data)
@@ -125,30 +137,31 @@ app.get('/shows-trakt', (req, res, next) => {
 })
 
 app.get('/shows-trakt/:id', (req, res, next) => {
+  //return Bad Request error if type is not given
   if (Object.keys(req.query).length === 0){
     const errorMessage = {
-    status: 412,
-    error:"Client Error",
-    message: "Precondition failed: content type (show or movie) is not indicated e.g. /shows-trakt/1390/?type=movie",
+    status: 400,
+    error:"Bad Request - request couldn't be parsed",
+    message: "Content type (show or movie) is not indicated e.g. /shows-trakt/1390/?type=movie",
     path: "/shows-trakt/:id",
   }
   res.json(errorMessage);
-  /*
-    axios
-      .get(
-        `https://api.trakt.tv/search/trakt/${req.params.id}?Content-Type=application/json&trakt-api-version=2&trakt-api-key=${process.env.API_KEY_TRAKT}`
-      )  
-      .then((response) => {
-        res.json(response.data)
-      })
-      .catch((err) => {
-        next(err)
-      })
-  */
+  //return extended show information 
   }else if (req.query.type=='show'){
     axios
       .get(
-        `https://api.trakt.tv/shows/${req.params.id}?extended=full&Content-Type=application/json&trakt-api-version=2&trakt-api-key=${process.env.API_KEY_TRAKT}`
+        `https://api.trakt.tv/shows/${req.params.id}`,{
+          params: {
+            'extended':'full',
+          },
+          headers: {
+            'trakt-api-version':'2', 
+            'trakt-api-key':`${process.env.API_KEY_TRAKT}`,
+            'extended':'full',
+            'Accept':'*/*', //necessary since requests have multiple content-types
+            'User-Agent':'request'
+          }
+        }
       )
       .then((response) => {
         res.json(response.data)
@@ -156,10 +169,22 @@ app.get('/shows-trakt/:id', (req, res, next) => {
       .catch((err) => {
         next(err)
       })
+  //return extended movie information     
   }else if (req.query.type=='movie'){
     axios
       .get(
-        `https://api.trakt.tv/movies/${req.params.id}?extended=full&Content-Type=application/json&trakt-api-version=2&trakt-api-key=${process.env.API_KEY_TRAKT}`
+        `https://api.trakt.tv/movies/${req.params.id}`,{
+          params: {
+            'extended':'full',
+          },
+          headers: { 
+            'trakt-api-version':'2', 
+            'trakt-api-key':`${process.env.API_KEY_TRAKT}`,
+            'extended':'full',
+            'Accept':'*/*',
+            'User-Agent':'request'
+          }
+        }
       )
       .then((response) => {
         res.json(response.data)
@@ -167,6 +192,15 @@ app.get('/shows-trakt/:id', (req, res, next) => {
       .catch((err) => {
         next(err)
       })
+  //return Not Found error if content is not found 
+  }else{
+      const errorMessage = {
+      status: 404,
+      error:"Not Found - method exists, but no record found",
+      message: "Content with the indicated Trakt id is not found",
+      path: "/shows-trakt/:id"
+      }
+    res.json(errorMessage);
   }
 })
 
