d9f274d13adaf6ffb79055134f9a78c443f0e2d0
Ben Kaplan <benrkaplan@gmail.com>
2021-04-05 01:30:18 -0400

    writing login post route and passport setup

--- back-end/app.js
+++ back-end/app.js
@@ -2,10 +2,38 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 const morgan = require("morgan") // middleware for logging of incoming HTTP requests
+const passport = require('passport'); //authentication middleware
+const LocalStrategy = require('passport-local').Strategy;
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan("dev")) // dev is a concise color-coded default style for morgan
+
+//=========set up passport auth============================
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function(user, done) { //store user id in passport
+	done(null, user._id);
+});
+passport.deserializeUser(function(userId, done) { //fetch user from database using id
+	User.findById(userId, (err, user) => done(err, user));
+});
+//local authentication strategy:
+//		* check if user is in database
+//		* check if hash of submitted password matches stored hash
+//		* call done or false
+const local = new LocalStrategy((username, password, done) => {
+	User.findOne( {username} )
+		.then(user => {
+			if (!user || !user.validPassword(password)) {
+				done(null, false);
+			} else {
+				done(null, user);
+			}
+		})
+		.catch(e => done(e));
+});
+passport.use('local', local);
 
 app.get('/tv_users', (req, res, next) => {
     axios.get(`https://my.api.mockaroo.com/tv_users.json?key=${process.env.MOCKAROO_KEY}`)
@@ -15,6 +43,28 @@ app.get('/tv_users', (req, res, next) => {
         .catch( (err) => {
             next(err)
         })
+});
+
+app.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+		return res.status(500).json({error: 'Issue with Passport authentication1'});
+	}
+    if (!user) {
+		return res.status(403).json({error: 'The login information entered is not correct. Please try again'});
+	}
+	if (user.mfaEnabled) {
+		sig_request = duo_web.sign_request(process.env.DUO_INTEGRATION_KEY, process.env.DUO_SECRET_KEY, process.env.DUO_APPLICATION_KEY, user.username);
+		return res.status(200).json({mfa: sig_request})
+	}
+    req.logIn(user, function(err) {
+      if (err) {
+		return res.status(500).json({error: 'Issue with Passport authentication2'});
+	  }
+	  	//console.log(req.user.stripeAccountId);
+		return res.json({success: 'Successfully logged in user'});
+    });
+  })(req, res, next);
 });
 
 app.get('/shows/:id', (req, res, next) => {
