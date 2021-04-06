c0ebdfb2016ac7278e3686f9345655b2a8017875
sam-eng <sme388@nyu.edu>
2021-04-05 23:14:32 -0400

    fix patch request and tests

--- back-end/app.js
+++ back-end/app.js
@@ -1,30 +1,34 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
-const morgan = require("morgan") // middleware for logging of incoming HTTP requests
-const { mockAllShows, mockShowAPI, createMockUser, mockUserAPI } = require('./MockData')
+const morgan = require('morgan') // middleware for logging of incoming HTTP requests
+const {
+  mockAllShows,
+  mockShowAPI,
+  createMockUser,
+  mockUserAPI
+} = require('./MockData')
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(morgan("dev")) // dev is a concise color-coded default style for morgan
+app.use(morgan('dev')) // dev is a concise color-coded default style for morgan
 
 app.get('/tv_users', (req, res, next) => {
   axios
-    .get(
-      `https://my.api.mockaroo.com/tv_users.json?key=`
-    )
+    .get(`https://my.api.mockaroo.com/tv_users.json?key=`)
     .then((response) => {
       res.json(response.data)
     })
     .catch((err) => {
       if (err.response.status === 500) {
-        res.status(200).json([createMockUser(1), createMockUser(2), createMockUser(3)])
-      }
-      else {
+        res
+          .status(200)
+          .json([createMockUser(1), createMockUser(2), createMockUser(3)])
+      } else {
         next(err)
       }
     })
-});
+})
 
 app.get('/shows/:id', (req, res, next) => {
   axios
@@ -38,19 +42,17 @@ app.get('/shows/:id', (req, res, next) => {
       if (err.response.status === 500) {
         if (req.params.id in mockShowAPI) {
           res.status(200).json(mockShowAPI[req.params.id])
-        }
-        else {
+        } else {
           res.status(404).json('show with requested id not found')
         }
-      }
-      else {
+      } else {
         next(err)
       }
     })
 })
 
 app.get('/', (req, res, next) => {
-  res.send("TV Tracker App Home page!")
+  res.send('TV Tracker App Home page!')
 })
 
 app.get('/tv_users/:id', (req, res, next) => {
@@ -65,35 +67,42 @@ app.get('/tv_users/:id', (req, res, next) => {
       if (err.response.status === 500) {
         if (req.params.id in mockUserAPI) {
           res.status(200).json(mockUserAPI[req.params.id])
-        }
-        else {
+        } else {
           res.status(400).json('user with requested id not found')
         }
-      }
-      else {
+      } else {
         next(err)
       }
     })
 })
 
 app.post('/tv_users', (req, res, next) => {
-    axios.post(`https://my.api.mockaroo.com/tv_users.json?key=&__method=POST`, {
-        "username": req.body.username,
-        "email": req.body.email,
-        "password": req.body.password
+  axios
+    .post(`https://my.api.mockaroo.com/tv_users.json?key=&__method=POST`, {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
     })
     .then((response) => {
       res.json(response.data)
     })
     .catch((err) => {
       if (err.response.status === 500) {
-        res.status(200).json(createMockUser(1, req.body.username, req.body.password, req.body.email))
-      }
-      else {
+        res
+          .status(200)
+          .json(
+            createMockUser(
+              1,
+              req.body.username,
+              req.body.password,
+              req.body.email
+            )
+          )
+      } else {
         next(err)
       }
     })
-});
+})
 
 app.get('/shows', (req, res, next) => {
   axios
@@ -106,25 +115,21 @@ app.get('/shows', (req, res, next) => {
     .catch((err) => {
       if (err.response.status === 500) {
         res.status(200).json(mockAllShows)
-      }
-      else {
+      } else {
         next(err)
       }
     })
 })
 
 app.patch('/tv_users/:id', (req, res, next) => {
+  const patchUser = {}
+  Object.keys(req.body).map((key) => {
+    patchUser[key] = req.body[key]
+  })
   axios
     .patch(
       `https://my.api.mockaroo.com/tv_users/${req.params.id}.json?key=${process.env.API_KEY_MOCKAROO}&__method=PATCH`,
-      {
-        username:
-          req.body.username == null ? req.query.username : req.body.username,
-        password:
-          req.body.password == null ? req.query.password : req.body.password,
-        bio: req.body.bio == null ? req.query.bio : req.body.bio,
-        shows: req.body.shows == null ? req.query.shows : req.body.shows
-      }
+      patchUser
     )
     .then((response) => {
       res.json(response.data)
@@ -136,19 +141,19 @@ app.patch('/tv_users/:id', (req, res, next) => {
 
 app.get('/shows-trakt', (req, res, next) => {
   //return popular shows if query is not given
-  if (Object.keys(req.query).length === 0){
+  if (Object.keys(req.query).length === 0) {
     axios
       .get(
         `https://api.trakt.tv/shows/popular?Content-Type=application/json&trakt-api-version=2&trakt-api-key=${process.env.API_KEY_TRAKT}`
-      )  
+      )
       .then((response) => {
         res.json(response.data)
       })
       .catch((err) => {
         next(err)
       })
-  //otherwise return requested show information
-  }else{
+    //otherwise return requested show information
+  } else {
     axios
       .get(
         `https://api.trakt.tv/search/show,movie?query=${req.query.query}&Content-Type=application/json&trakt-api-version=2&trakt-api-key=${process.env.API_KEY_TRAKT}`
@@ -163,8 +168,8 @@ app.get('/shows-trakt', (req, res, next) => {
 })
 
 app.get('/logout', (req, res, next) => {
-	//req.logOut(); add later when database is setup
-	res.json({success: "Successfully logged out"});
+  //req.logOut(); add later when database is setup
+  res.json({ success: 'Successfully logged out' })
 })
 
 module.exports = app
--- back-end/test/patchUsers.js
+++ back-end/test/patchUsers.js
@@ -6,42 +6,70 @@ const sinon = require('sinon')
 var expect = chai.expect
 chai.use(chaiHttp)
 
-describe('PATCH /tv_users/1', () => { //update all fields of user info
-  it('should update all user info fields', async () => {
-    const stub = sinon.stub(axios, 'patch').resolves({data: {
-      username: 'newUsername', 
-      password: 'newPassword',
-      email: 'newemail@gmail.com',
-      shows: 'new show 1'}})
-    const res = await chai.request(server).patch('/tv_users/1')
-    expect(res.status).to.equal(200)
-    expect(res.body).to.deep.equal({
-      username: 'newUsername', 
-      password: 'newPassword',
-      email: 'newemail@gmail.com',
-      shows: 'new show 1'})
-      stub.restore()
-    })
-  it('should update only the password', async () => { //update 1 field of user info
-    const stub = sinon.stub(axios, 'patch').resolves({data: {
-      username: null,
-      password: 'newPassword',
-      email: null,
-      shows: null
-    }})
-    const res = await chai.request(server).patch('/tv_users/1')
-    expect(res.status).to.equal(200)
-    expect(res.body).to.deep.equal({
-      username: res.body.username, 
-      password: 'newPassword',
-      email: res.body.email,
-      shows: res.body.shows})
-      stub.restore()
-    })
-  it('should return 500', async () => { //error
-    const stub = sinon.stub(axios, 'patch').rejects(true)
-    const res = await chai.request(server).patch('/tv_users/1')
-    expect(res.status).to.equal(500)
+describe('PATCH /tv_users/1', () => {
+  const patchURL = `https://my.api.mockaroo.com/tv_users/1.json?key=${process.env.API_KEY_MOCKAROO}&__method=PATCH`
+  let stub
+
+  afterEach(() => {
     stub.restore()
+  })
+
+  it('should update all user info fields', async () => {
+    stub = sinon.stub(axios, 'patch').resolves({
+      data: {
+        username: 'newUsername',
+        password: 'newPassword',
+        email: 'newemail@gmail.com',
+        bio: 'old bio',
+        shows: [{ id: 1, name: 'sample show' }]
+      }
     })
+    const res = await chai
+      .request(server)
+      .patch('/tv_users/1')
+      .send({
+        username: 'newUsername',
+        password: 'newPassword',
+        email: 'newemail@gmail.com',
+        shows: [{ id: 1, name: 'sample show' }]
+      })
+    expect(res.status).to.equal(200)
+    expect(res.body).to.deep.equal({
+      username: 'newUsername',
+      password: 'newPassword',
+      email: 'newemail@gmail.com',
+      bio: 'old bio',
+      shows: [{ id: 1, name: 'sample show' }]
+    })
+    sinon.assert.calledOnce(stub)
+    sinon.assert.calledWith(stub, patchURL, {
+      username: 'newUsername',
+      password: 'newPassword',
+      email: 'newemail@gmail.com',
+      shows: [{ id: 1, name: 'sample show' }]
+    })
+  })
+  it('should update only the password', async () => {
+    stub = sinon.stub(axios, 'patch').resolves({
+      data: {
+        username: 'old username',
+        password: 'newPassword',
+        email: 'oldEmail@gmail.com',
+        shows: []
+      }
+    })
+    const res = await chai
+      .request(server)
+      .patch('/tv_users/1')
+      .send({ password: 'newPassword' })
+    expect(res.status).to.equal(200)
+    expect(res.body).to.deep.equal({
+      username: 'old username',
+      password: 'newPassword',
+      email: 'oldEmail@gmail.com',
+      shows: []
+    })
+    sinon.assert.calledOnce(stub)
+    sinon.assert.calledWith(stub, patchURL, { password: 'newPassword' })
+  })
 })
