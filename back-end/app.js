0b6ef2c71c255aebf43314f5ea9d39719d9e1396
Sam Eng <35475933+sam-eng@users.noreply.github.com>
2021-04-06 08:36:14 -0400

    Return mock data from API when Mockaroo gives 500 (#231)
    * add mocked data for get /shows
    
    * add mock data to get shows/id
    
    * add mock data to get /tv_users
    
    * add more mockaroo data to be returned by endpoints
    
    * add mocked data for post users
    
    * update gitignore
    
    * fix patch request and tests
    
    * prettier's formatting for tests
    
    * alter MockData to fit PATCH request
    
    * return mocked data for patch
    
    * make tests for 500 mockaroo error work
    
    * move mock error message to MockData.js
    
    * add tests for mock data cases of get /show/id
    
    * add mockaroo 500 error test for get /shows
    
    * assert that stub is called once for tests
    
    * re-add api key
    
    * touch up comments

--- back-end/app.js
+++ back-end/app.js
@@ -1,21 +1,38 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
-const morgan = require("morgan") // middleware for logging of incoming HTTP requests
+const morgan = require('morgan') // middleware for logging of incoming HTTP requests
+const {
+  mockAllShows,
+  mockShowAPI,
+  createMockUser,
+  mockUserAPI,
+  mockUserUpdate
+} = require('./MockData')
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(morgan("dev")) // dev is a concise color-coded default style for morgan
+app.use(morgan('dev')) // dev is a concise color-coded default style for morgan
 
 app.get('/tv_users', (req, res, next) => {
-    axios.get(`https://my.api.mockaroo.com/tv_users.json?key=${process.env.MOCKAROO_KEY}`)
-        .then( (response) => {
-            res.json(response.data)
-        })
-        .catch( (err) => {
-            next(err)
-        })
-});
+  axios
+    .get(
+      `https://my.api.mockaroo.com/tv_users.json?key=${process.env.API_KEY_MOCKAROO}`
+    )
+    .then((response) => {
+      res.json(response.data)
+    })
+    .catch((err) => {
+      // This specifically is for Mockaroo errors
+      if (err.response.status === 500) {
+        res
+          .status(200)
+          .json([createMockUser(1), createMockUser(2), createMockUser(3)])
+      } else {
+        next(err)
+      }
+    })
+})
 
 app.get('/shows/:id', (req, res, next) => {
   axios
@@ -26,12 +43,20 @@ app.get('/shows/:id', (req, res, next) => {
       res.json(response.data)
     })
     .catch((err) => {
-      next(err)
+      if (err.response.status === 500) {
+        if (req.params.id in mockShowAPI) {
+          res.status(200).json(mockShowAPI[req.params.id])
+        } else {
+          res.status(404).json('show with requested id not found')
+        }
+      } else {
+        next(err)
+      }
     })
 })
 
 app.get('/', (req, res, next) => {
-  res.send("TV Tracker App Home page!")
+  res.send('TV Tracker App Home page!')
 })
 
 app.get('/tv_users/:id', (req, res, next) => {
@@ -43,24 +68,48 @@ app.get('/tv_users/:id', (req, res, next) => {
       res.json(response.data)
     })
     .catch((err) => {
-      next(err)
+      if (err.response.status === 500) {
+        if (req.params.id in mockUserAPI) {
+          res.status(200).json(mockUserAPI[req.params.id])
+        } else {
+          res.status(400).json('user with requested id not found')
+        }
+      } else {
+        next(err)
+      }
     })
 })
 
 app.post('/tv_users', (req, res, next) => {
-    axios.post(`https://my.api.mockaroo.com/tv_users.json?key=${process.env.API_KEY_MOCKAROO}&__method=POST`, {
-        "username": req.body.username,
-        "email": req.body.email,
-        "password": req.body.password
-    })
+  axios
+    .post(
+      `https://my.api.mockaroo.com/tv_users.json?key=${process.env.API_KEY_MOCKAROO}&__method=POST`,
+      {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+      }
+    )
     .then((response) => {
-        console.log(response)
-        res.json(response.data)
+      res.json(response.data)
     })
     .catch((err) => {
+      if (err.response.status === 500) {
+        res
+          .status(200)
+          .json(
+            createMockUser(
+              1,
+              req.body.username,
+              req.body.password,
+              req.body.email
+            )
+          )
+      } else {
         next(err)
+      }
     })
-});
+})
 
 app.get('/shows', (req, res, next) => {
   axios
@@ -71,46 +120,51 @@ app.get('/shows', (req, res, next) => {
       res.json(response.data)
     })
     .catch((err) => {
-      next(err)
+      if (err.response.status === 500) {
+        res.status(200).json(mockAllShows)
+      } else {
+        next(err)
+      }
     })
 })
 
 app.patch('/tv_users/:id', (req, res, next) => {
+  const patchUser = {}
+  Object.keys(req.body).map((key) => {
+    patchUser[key] = req.body[key]
+  })
   axios
     .patch(
       `https://my.api.mockaroo.com/tv_users/${req.params.id}.json?key=${process.env.API_KEY_MOCKAROO}&__method=PATCH`,
-      {
-        username:
-          req.body.username == null ? req.query.username : req.body.username,
-        password:
-          req.body.password == null ? req.query.password : req.body.password,
-        bio: req.body.bio == null ? req.query.bio : req.body.bio,
-        shows: req.body.shows == null ? req.query.shows : req.body.shows
-      }
+      patchUser
     )
     .then((response) => {
       res.json(response.data)
     })
     .catch((err) => {
-      next(err)
+      if (err.response.status == 500) {
+        res.status(200).json(mockUserUpdate(req.params.id, patchUser))
+      } else {
+        next(err)
+      }
     })
 })
 
 app.get('/shows-trakt', (req, res, next) => {
-  //return popular shows if query is not given
-  if (Object.keys(req.query).length === 0){
+  //Return a list of popular shows if search query is not given
+  if (Object.keys(req.query).length === 0) {
     axios
       .get(
         `https://api.trakt.tv/shows/popular?Content-Type=application/json&trakt-api-version=2&trakt-api-key=${process.env.API_KEY_TRAKT}`
-      )  
+      )
       .then((response) => {
         res.json(response.data)
       })
       .catch((err) => {
         next(err)
       })
-  //otherwise return requested show information
-  }else{
+    //otherwise return requested show information
+  } else {
     axios
       .get(
         `https://api.trakt.tv/search/show,movie?query=${req.query.query}&Content-Type=application/json&trakt-api-version=2&trakt-api-key=${process.env.API_KEY_TRAKT}`
@@ -125,8 +179,8 @@ app.get('/shows-trakt', (req, res, next) => {
 })
 
 app.get('/logout', (req, res, next) => {
-	//req.logOut(); add later when database is setup
-	res.json({success: "Successfully logged out"});
+  //req.logOut(); add later when database is setup
+  res.json({ success: 'Successfully logged out' })
 })
 
 module.exports = app
--- back-end/test/getShows.js
+++ back-end/test/getShows.js
@@ -4,22 +4,32 @@ const axios = require('axios')
 const sinon = require('sinon')
 const expect = chai.expect
 const server = require('../app.js')
+const { mockErrorMessage, mockAllShows } = require('../MockData.js')
 chai.use(chaiHttp)
 
 describe('GET /shows', () => {
-  it('should return 200 OK', async () => {
-    // This effectively makes response.data in the show route equal to 
-    // {id: 1, name: 'sample show}
-    const stub = sinon.stub(axios, 'get').resolves({data: {id: 1, name: 'sample show'}})
-    const res = await chai.request(server).get('/shows')
-    expect(res.status).to.equal(200)
-    expect(res.body).to.deep.equal({id: 1, name: 'sample show'})
+  let stub
+
+  afterEach(() => {
     stub.restore()
   })
-  it('should return 500 error', async () => {
-    const stub = sinon.stub(axios, 'get').rejects(true)
+
+  it('should return 200 OK', async () => {
+    // This effectively makes response.data in the show route equal to
+    // {id: 1, name: 'sample show}
+    stub = sinon
+      .stub(axios, 'get')
+      .resolves({ data: { id: 1, name: 'sample show' } })
     const res = await chai.request(server).get('/shows')
-    expect(res.status).to.equal(500)
-    stub.restore()
+    expect(res.status).to.equal(200)
+    expect(res.body).to.deep.equal({ id: 1, name: 'sample show' })
+    sinon.assert.calledOnce(stub)
+  })
+  it('should return mocked data when stubbed Mockaroo call results in 500 error', async () => {
+    stub = sinon.stub(axios, 'get').rejects(mockErrorMessage)
+    const res = await chai.request(server).get('/shows')
+    expect(res.status).to.equal(200)
+    expect(res.body).to.deep.equal(mockAllShows)
+    sinon.assert.calledOnce(stub)
   })
 })
