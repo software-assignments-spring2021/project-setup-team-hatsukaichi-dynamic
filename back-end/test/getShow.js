7b4a82423babe026f2c9ad0eecbc8729bcd3a86f
sam-eng <sme388@nyu.edu>
2021-04-06 00:04:41 -0400

    add tests for mock data cases of get /show/id

--- back-end/test/getShow.js
+++ back-end/test/getShow.js
@@ -4,16 +4,36 @@ const axios = require('axios')
 const sinon = require('sinon')
 const expect = chai.expect
 const server = require('../app.js')
+const { mockErrorMessage, mockShowAPI } = require('../MockData.js')
 chai.use(chaiHttp)
 
 describe('GET /shows/1', () => {
-  it('should return 200 OK', async () => {
-    const stub = sinon
+  let stub
+
+  afterEach(() => {
+    stub.restore()
+  })
+
+  it('should return 200 OK and data', async () => {
+    stub = sinon
       .stub(axios, 'get')
       .resolves({ data: { id: 1, name: 'sample show' } })
     const res = await chai.request(server).get('/shows/1')
     expect(res.status).to.equal(200)
     expect(res.body).to.deep.equal({ id: 1, name: 'sample show' })
-    stub.restore()
+  })
+  describe('when Mockaroo returns with 500 error', () => {
+    it('should return 200 with mock data if show exists', async () => {
+      stub = sinon.stub(axios, 'get').rejects(mockErrorMessage)
+      const res = await chai.request(server).get('/shows/3')
+      expect(res.status).to.equal(200)
+      expect(res.body).to.deep.equal(mockShowAPI[3])
+    })
+    it('should return 404 with error message if show does not exist', async () => {
+      stub = sinon.stub(axios, 'get').rejects(mockErrorMessage)
+      const res = await chai.request(server).get('/shows/2')
+      expect(res.status).to.equal(404)
+      expect(res.body).to.deep.equal('show with requested id not found')
+    })
   })
 })
