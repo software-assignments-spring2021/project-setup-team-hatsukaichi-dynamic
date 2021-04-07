63c8e3fbf72f180e6a768205d5b94460fc6eede6
Almazhan (Alma) Kapan <aa5456@nyu.edu>
2021-04-07 09:14:01 +0800

    Unit tests for Post Users route (#237)
    * add post users unit test
    
    * add changes from review
    
    * use createMockUser, update the 200 error check
    
    * fix grammar

---
+++ back-end/test/postUsers.js
@@ -0,0 +1,71 @@
+const server = require('../app.js')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const axios = require('axios')
+const sinon = require('sinon')
+const { createMockUser } = require('../MockData.js')
+var expect = chai.expect
+chai.use(chaiHttp)
+
+describe('POST /tv_users', () => {
+  const postURL = `https://my.api.mockaroo.com/tv_users.json?key=${process.env.API_KEY_MOCKAROO}&__method=POST`
+  let stub
+
+  afterEach(() => {
+    stub.restore()
+  })
+
+  it('should create all user info fields', async () => {
+    stub = sinon.stub(axios, 'post').resolves({
+      data: {
+        id: 'sampleID',
+        username: 'newUsername',
+        password: 'newPassword',
+        email: 'newemail@gmail.com',
+        bio: '',
+        img: '',
+        shows: []
+      }
+    })
+    const res = await chai
+      .request(server)
+      .post('/tv_users')
+      .send({
+        username: 'newUsername',
+        password: 'newPassword',
+        email: 'newemail@gmail.com',
+      })
+    expect(res.status).to.equal(200)
+    expect(res.body).to.deep.equal({
+      id: 'sampleID',
+      username: 'newUsername',
+      password: 'newPassword',
+      email: 'newemail@gmail.com',
+      bio: '',
+      img: '',
+      shows: []
+    })
+    sinon.assert.calledOnce(stub)
+    sinon.assert.calledWith(stub, postURL, {
+      username: 'newUsername',
+      password: 'newPassword',
+      email: 'newemail@gmail.com',
+    })
+  })
+  it('should return new data when stubbed Mockaroo call results in 500 error', async () => {
+    stub = sinon.stub(axios, 'post').rejects({
+      response: {
+        status: 500,
+        message: 'mockaroo api limit exceeded (probably)'
+      }
+    })
+    const res = await chai
+      .request(server)
+      .post('/tv_users')
+      .send({ username: 'test user', password: 'test password', email: 'testEmail@gmail.com'})
+    expect(res.status).to.equal(200)
+    expect(res.body).to.deep.equal(createMockUser(1, 'test user', 'test password', 'testEmail@gmail.com'))
+    sinon.assert.calledOnce(stub)
+    sinon.assert.calledWith(stub, postURL, { username: 'test user', password: 'test password', email: 'testEmail@gmail.com'})
+  })
+})
