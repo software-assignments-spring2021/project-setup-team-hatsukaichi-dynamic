7f400fdbf0fb9eab181ab75e2318f967a6ceff82
Mason Kalaty <mk6578@nyu.edu>
2021-04-14 00:52:17 -0400

    add onFirstCall

---
+++ back-end/test/getShowTrakt.js
@@ -0,0 +1,32 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const axios = require('axios')
+const sinon = require('sinon')
+const expect = chai.expect
+const server = require('../app.js')
+const { mockErrorMessage, mockShowAPI } = require('../MockData.js')
+chai.use(chaiHttp)
+
+describe('GET /shows/1', () => {
+  let stub
+
+  afterEach(() => {
+    stub.restore()
+  })
+
+  it('should return 200 OK and data', async () => {
+    stub = sinon
+      .stub(axios, 'get')
+      .onFirstCall().resolves({
+        data:
+          {
+            mockGOT
+          }
+      })
+      .onSecondCall().resolves(2)
+    const res = await chai.request(server).get('/shows/1')
+    expect(res.status).to.equal(200)
+    expect(res.body).to.deep.equal({ id: 1, name: 'sample show' })
+    sinon.assert.calledOnce(stub)
+  })
+})
