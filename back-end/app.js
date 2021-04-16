230ef730a40a21811fc3e2bdc4a85917952057b8
LevBernstein <lev2000@me.com>
2021-04-16 15:05:50 -0400

    No error logging

--- back-end/app.js
+++ back-end/app.js
@@ -210,7 +210,6 @@ app.patch(
   (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      console.log(errors)
       return res.status(400).json({ errors: errors.array() })
     }
     const patchUser = {}
