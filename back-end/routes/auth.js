eb73101c154ffcb94a4eb6dba3aec74447749bd9
Almazhan Kapan <aa5456@nyu.edu>
2021-04-20 15:51:56 -0400

    hash password, add more validation

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,5 +1,6 @@
 const app = require('express').Router()
 const User=require('../models/User')
+const bcrypt = require('bcrypt') //encrypt password
 
 //validating user post
 
@@ -28,19 +29,30 @@ const loginValidation = (data) => {
   return schema.validate(data)
 }
 
-
 app.post('/register', async (req, res) => {
   //validate data
   const {error} =registerValidation(req.body)
-  res.send(error.error.details[0].message)
+  
   //send validation error
-  if (error) return res.status(400).send(error.error.details[0].message)
+  if (error) return res.status(400).send(error.details[0].message)
+  
+  //check if email exists
+  const emailExist = await User.findOne({email: req.body.email})
+  if (emailExist) return res.status(400).send('Email already exists')
+
+  //check if username exists
+  const usernameExist = await User.findOne({email: req.body.username})
+  if (usernameExist) return res.status(400).send('Username already exists')
+
+  //hash password
+  const salt= await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(req.body.passwordHash, salt)
 
   const user = new User({
     id: req.body.id, 
     username: req.body.username, 
     email: req.body.email, 
-    passwordHash: req.body.passwordHash
+    passwordHash: hashedPassword
   })
   try{
     const savedUser=await user.save()
