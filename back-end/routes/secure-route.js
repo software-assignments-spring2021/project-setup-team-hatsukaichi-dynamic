c974581c9e4117f26aee2cb721b5395f6554d68a
Almazhan Kapan <aa5456@nyu.edu>
2021-04-23 15:01:16 -0400

    add comment to /profile route

--- back-end/routes/secure-route.js
+++ back-end/routes/secure-route.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express.Router();
+
+/*Note: /profile route is a placeholder route for secure access; 
+ /patch route will be moved to the file to replace the route */
+
 //to access the secure route, a query with a secret_token needs to be provided
 //for example: http://localhost:4000/profile?secret_token=XXXXX where XXXXX 
+
 app.get(
   '/profile',
   (req, res, next) => {
