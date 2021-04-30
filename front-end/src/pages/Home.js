597d437c834bce5ea25a3b34ce733f986dd6baca
Almazhan Kapan <aa5456@nyu.edu>
2021-05-01 05:29:55 +0800

    add gifs and description to the home, edit style

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import video from '../videos/instructions.webm'
+import profile from '../images/profile.gif'
+import myShows from '../images/shows.gif'
+import searchShows from '../images/shows-search.gif'
 import './Home.css'
 
 function Home() {
@@ -9,9 +11,52 @@ function Home() {
     <>
       <Header />
       <div className="home">
+        <div className="block">
         <h2 className="header-text">Welcome to TV Tracker app! </h2>
-        <video controls autoplay className="full-img" src={video}>
-        </video>
+        <h4 className="gif-text">Register and access Your Profile page: </h4>
+        <p className="full-text">After registering an account on TV Tracker App, 
+        you can access your personal Profile page! </p>
+         <p className="full-text"> On the profile page, you can: </p>
+       <p className="full-text">  1) Access Most Recently Watched shows </p> 
+       <p className="full-text">2) click on Individual show and modify watching progress of the show </p>
+        <p className="full-text">2) access My Shows page  </p>
+       <p className="full-text">3) access Profile Settings </p>
+        <img
+          src={profile}
+          alt="profile gif"
+          className="gif-img"
+        />
+        <br/>
+        </div>
+        <br/> <br/>
+     <div className="block">
+          <h4 className="gif-text">Access Shows page: </h4>
+  <p className="full-text">After registering an account on TV Tracker App, 
+  you can access your personal Shows page! </p>
+   <p className="full-text"> On the Shows page, you can: </p>
+ <p className="full-text">  1) Access all shows linked to your profile</p> 
+  <p className="full-text">2) click on Individual show and modify watching progress of the show </p>
+  <p className="full-text">2) Filter shows based on your progress (Completed, In Progress) </p>
+ <p className="full-text">3) Filter shows based on the platform (Netflix, HBO etc. ) </p>
+  <img
+    src={myShows}
+    alt="shows gif"
+    className="gif-img"
+  />
+   <br/>
+  </div>
+       <div className="block">
+          <h4 className="gif-text">Find and save your favorite shows: </h4>
+  <p className="full-text">On the Shows page, 
+  you can search for new shows and add them to your list </p>
+  <p className="full-text">Search for a show, modify your watching progress and save it</p>
+  <img
+    src={searchShows}
+     alt="search shows gif"
+    className="gif-img"
+  />
+   <br/>
+  </div>
         <p className="full-text">
           <br></br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
           do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque
