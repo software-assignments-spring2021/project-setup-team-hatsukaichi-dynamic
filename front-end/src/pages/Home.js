e9b5c5d47bd57ddfca14770ba31e479840b4a7b2
Almazhan Kapan <aa5456@nyu.edu>
2021-04-30 21:08:28 -0400

    add content for home

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -11,19 +11,32 @@ function Home() {
     <>
       <Header />
       <div className="home">
-        <h2 className="header-text">Welcome to TV Tracker app! </h2>
+        <h2 className="header-text">Welcome to the TV Tracker App page! </h2>
         <div className="block">
           <p className="full-text">
             <br />
             <br />
-            In today's world, there are more streaming services and platforms
-            than we can count. Many people bite off more than they can chew and
-            try to watch multiple shows spread accross multiple platforms and
-            genres. It can be pretty tricky to keep mental track of your
-            progress on each show individually, that's where we come in. TV
-            Tracker is an online hub to keep note of the shows you watch or have
-            watched, your progress within those shows, the streaming platforms
-            that carry the shows, and much more.
+            In the world of countless shows and streaming services, it can
+            become very tedious to keep track of all the shows that you are
+            watching or have watched, their corresponding platforms, seasons and
+            episodes.{' '}
+          </p>
+          <p className="full-text">
+            {' '}
+            With
+            <b> TV Tracker App, </b> you can solve this problem with minimum
+            time and effort spent and get much more!
+            <br /> With our database of shows powered by{' '}
+            <a href="https://trakt.tv/about">Trakt.tv</a>, you can find and
+            update the shows that you are currently watching or have already
+            completed. You can also save and update the streaming platform,
+            season and episode info for each show.
+            <br />
+            <br /> Moreover, you can share your list of shows with friends and
+            also view what shows and movies are on their list.
+            <br />
+            Our App is free to use, so you only need a working email to register
+            :)
           </p>
         </div>
         <br /> <br />
@@ -34,18 +47,25 @@ function Home() {
             personal Profile page!{' '}
           </p>
           <p className="full-text"> On the profile page, you can: </p>
-          <p className="full-text"> 1. Access Most Recently Watched shows </p>
+          <p className="full-text"> 1. View Most Recently Added shows </p>
           <p className="full-text">
-            2. Click on Individual show and modify watching progress of the show{' '}
+            2. Click on each individual show and update the watching progress
+            for the show: <br /> - Whether you have already completed the show
+            or still watching <br /> - What season and episode you are watching{' '}
+            <br /> - Streaming service that you are watching the show on.{' '}
           </p>
-          <p className="full-text">3. Access My Shows page </p>
-          <p className="full-text">4. Access Profile Settings </p>
+          <p className="full-text">3. View your personal Shows page </p>
+          <p className="full-text">4. View and edit your Profile Settings </p>
+          <p className="full-text">
+            5. Copy your profile link to the clipboard so that you can easily
+            share it!{' '}
+          </p>
           <img src={profile} alt="profile gif" className="gif-img" />
           <br />
         </div>
         <br /> <br />
         <div className="block">
-          <h4 className="gif-text">Access Shows page: </h4>
+          <h4 className="gif-text">View Shows page: </h4>
           <p className="full-text">
             After registering an account on TV Tracker App, you can access your
             personal Shows page!{' '}
@@ -53,10 +73,11 @@ function Home() {
           <p className="full-text"> On the Shows page, you can: </p>
           <p className="full-text">
             {' '}
-            1. Access all shows linked to your profile
+            1. View all shows linked to your account.
           </p>
           <p className="full-text">
-            2. Click on Individual show and modify watching progress of the show{' '}
+            2. Click on each individual show and update the watching progress of
+            the show{' '}
           </p>
           <p className="full-text">
             3. Filter shows based on your progress (Completed, In Progress){' '}
@@ -75,11 +96,13 @@ function Home() {
             list{' '}
           </p>
           <p className="full-text">
-            Search for a show, modify your watching progress and save it
+            For each individual show, you can update your watching progress and
+            save it
           </p>
           <img src={searchShows} alt="search shows gif" className="gif-img" />
           <br />
         </div>
+        
         <br />
       </div>
       <Footer />
