5188b6f9b784505830782a9c3d3800f59108dbbf
sam-eng <sme388@nyu.edu>
2021-05-06 20:13:14 -0400

    attempt at sticky header

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import 'react-sticky-header/styles.css'
 import StickyHeader from 'react-sticky-header'
 import './Header.css'
@@ -12,27 +12,43 @@ import LogoutButton from './LogoutButton'
 
 const Header = () => {
   const { loggedInUser } = React.useContext(AuthContext)
+  const [isSticky, setSticky] = useState(false)
+  const wrapper = useRef(null)
+  const handleScroll = () => {
+    if (wrapper.current) {
+      setSticky(wrapper.current.getBoundingClientRect().top <= 0)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', () => handleScroll)
+    }
+  }, [])
+
   return (
-    <StickyHeader
-      header={
-        <div className="header-root">
-          <Hamburger
-            pageWrapId={'page-wrap'}
-            outerContainerId={'outer-container'}
-          />
-          <HeaderLogo src={logo} className="logo-img" />
-          <div className="login-signup-buttons">
-            {loggedInUser ? (
-              <LogoutButton />
-            ) : (
-              <>
-                <LoginButton />
-                <SignupButton />
-              </>
-            )}
-          </div>
+    <div
+      className={`header-root sticky-wrapper${isSticky ? ' sticky' : ''}`}
+      ref={wrapper}>
+      <div className="sticky-inner">
+        <Hamburger
+          pageWrapId={'page-wrap'}
+          outerContainerId={'outer-container'}
+        />
+        <HeaderLogo src={logo} className="logo-img" />
+        <div className="login-signup-buttons">
+          {loggedInUser ? (
+            <LogoutButton />
+          ) : (
+            <>
+              <LoginButton />
+              <SignupButton />
+            </>
+          )}
         </div>
-      }></StickyHeader>
+      </div>
+    </div>
   )
 }
 
